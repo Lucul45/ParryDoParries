61f56c438948e17e86fb5b94856b01a46710d0ff
Lucul45 <71066283+Lucul45@users.noreply.github.com>
2025-05-23 23:33:59 +0200

    keyboard input not functionnal

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D _rb;
+
+    [SerializeField] private float _playerSpeed = 10f;
+
+    private Vector2 _movementInput = Vector2.zero;
+    private bool _attacking = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move(_movementInput);
+
+        if (_attacking)
+        {
+            Debug.Log("ATTACK");
+        }
+    }
+
+    private void Move(Vector2 dir)
+    {
+        _rb.velocity = new Vector2(dir.x * _playerSpeed, 0);
+    }
+
+    public void GetMovementInput(InputAction.CallbackContext context)
+    {
+        _movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void GetAttackInput(InputAction.CallbackContext context)
+    {
+        _attacking = context.action.IsPressed();
     }
 }
